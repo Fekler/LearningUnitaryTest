f87e539ac8ca06f6ca01af75b3bff7a8ba65e5b4
Felipe Queiroz <fekler.jobs@gmail.com>
2023-05-12 14:34:29 -0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TDD
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
